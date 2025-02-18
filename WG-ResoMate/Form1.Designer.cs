15bf33ed21d44e6eac9004322bd9a4620123e7f5
PangeranWiguan <pangeranwiguan@gmail.com>
2025-02-18 23:20:37 +0800

    Version 0.1
    Working condition quick change to resolution 1080p and 4K.

---
+++ WG-ResoMate/Form1.Designer.cs
@@ -0,0 +1,90 @@
+﻿namespace WG_ResoMate
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn1080p = new System.Windows.Forms.Button();
+            this.btn4K = new System.Windows.Forms.Button();
+            this.footer = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn1080p
+            // 
+            this.btn1080p.Location = new System.Drawing.Point(114, 161);
+            this.btn1080p.Name = "btn1080p";
+            this.btn1080p.Size = new System.Drawing.Size(101, 23);
+            this.btn1080p.TabIndex = 0;
+            this.btn1080p.Text = "Set 1920x1080";
+            this.btn1080p.UseVisualStyleBackColor = true;
+            this.btn1080p.Click += new System.EventHandler(this.btn1080p_Click);
+            // 
+            // btn4K
+            // 
+            this.btn4K.Location = new System.Drawing.Point(493, 161);
+            this.btn4K.Name = "btn4K";
+            this.btn4K.Size = new System.Drawing.Size(75, 23);
+            this.btn4K.TabIndex = 1;
+            this.btn4K.Text = "Set 4K";
+            this.btn4K.UseVisualStyleBackColor = true;
+            this.btn4K.Click += new System.EventHandler(this.btn4K_Click);
+            // 
+            // footer
+            // 
+            this.footer.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Bottom | System.Windows.Forms.AnchorStyles.Right)));
+            this.footer.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.footer.Cursor = System.Windows.Forms.Cursors.No;
+            this.footer.Location = new System.Drawing.Point(596, 425);
+            this.footer.Name = "footer";
+            this.footer.ReadOnly = true;
+            this.footer.Size = new System.Drawing.Size(192, 13);
+            this.footer.TabIndex = 4;
+            this.footer.Text = "Version 0.1 © 2025 Pangeran Wiguan";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.footer);
+            this.Controls.Add(this.btn4K);
+            this.Controls.Add(this.btn1080p);
+            this.Name = "Form1";
+            this.Text = "WG-ResoMate";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn1080p;
+        private System.Windows.Forms.Button btn4K;
+        private System.Windows.Forms.TextBox footer;
+    }
+}
+
---
+++ WG-ResoMate/Form1.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Runtime.InteropServices;
+using System.Windows.Forms;
+
+// Copyright Notice
+// Copyright (c) 2025 Pangeran Wiguan. All rights reserved.
+// WG-ResoMate: A tool to toggle between screen resolutions.
+
+namespace WG_ResoMate
+{
+    public partial class Form1 : Form
+    {
+        // Import necessary Windows API functions for changing display settings
+        [DllImport("user32.dll")]
+        private static extern int EnumDisplaySettings(string deviceName, int modeNum, ref DEVMODE devMode);
+
+        [DllImport("user32.dll")]
+        private static extern int ChangeDisplaySettings(ref DEVMODE devMode, int flags);
+
+        // Constants for display settings
+        private const int ENUM_CURRENT_SETTINGS = -1; // Retrieves the current display settings
+        private const int CDS_UPDATEREGISTRY = 0x01;  // Updates the registry with new settings
+        private const int DISP_CHANGE_SUCCESSFUL = 0; // Indicates success when changing display settings
+
+        // Struct representing the device mode (resolution, frequency, etc.)
+        [StructLayout(LayoutKind.Sequential)]
+        private struct DEVMODE
+        {
+            private const int CCHDEVICENAME = 32;
+            private const int CCHFORMNAME = 32;
+
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHDEVICENAME)]
+            public string dmDeviceName; // Device name
+            public short dmSpecVersion; // Version number
+            public short dmDriverVersion; // Driver version
+            public short dmSize; // Size of the DEVMODE structure
+            public short dmDriverExtra; // Extra driver data
+            public int dmFields; // Flags indicating which fields are valid
+            public int dmPositionX; // X position of the monitor
+            public int dmPositionY; // Y position of the monitor
+            public int dmDisplayOrientation; // Display orientation
+            public int dmDisplayFixedOutput; // Fixed output type
+            public short dmColor; // Color resolution
+            public short dmDuplex; // Duplex mode
+            public short dmYResolution; // Vertical resolution
+            public short dmTTOption; // TrueType options
+            public short dmCollate; // Collation
+            [MarshalAs(UnmanagedType.ByValTStr, SizeConst = CCHFORMNAME)]
+            public string dmFormName; // Form name
+            public short dmLogPixels; // Logical pixels (DPI)
+            public int dmBitsPerPel; // Bits per pixel (color depth)
+            public int dmPelsWidth; // Screen width in pixels
+            public int dmPelsHeight; // Screen height in pixels
+            public int dmDisplayFlags; // Display flags
+            public int dmDisplayFrequency; // Refresh rate
+            public int dmICMMethod; // ICM method
+            public int dmICMIntent; // ICM intent
+            public int dmMediaType; // Media type
+            public int dmDitherType; // Dither type
+            public int dmReserved1; // Reserved field
+            public int dmReserved2; // Reserved field
+            public int dmPanningWidth; // Panning width
+            public int dmPanningHeight; // Panning height
+        }
+
+        // Constructor for the form
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        /// <summary>
+        /// Changes the screen resolution using the Windows API.
+        /// Part of WG-ResoMate: A tool to toggle between screen resolutions.
+        /// </summary>
+        /// <param name="width">The desired screen width in pixels.</param>
+        /// <param name="height">The desired screen height in pixels.</param>
+        /// @since 2025-02-18
+        private void ChangeResolution(int width, int height)
+        {
+            DEVMODE dm = new DEVMODE(); // Create a new DEVMODE structure
+            dm.dmSize = (short)Marshal.SizeOf(typeof(DEVMODE)); // Set the size of the structure
+
+            // Retrieve the current display settings
+            if (EnumDisplaySettings(null, ENUM_CURRENT_SETTINGS, ref dm) != 0)
+            {
+                dm.dmPelsWidth = width; // Set the desired width
+                dm.dmPelsHeight = height; // Set the desired height
+
+                // Apply the new display settings
+                int result = ChangeDisplaySettings(ref dm, CDS_UPDATEREGISTRY);
+                if (result == DISP_CHANGE_SUCCESSFUL)
+                {
+                    MessageBox.Show("Resolution changed successfully.");
+                }
+                else
+                {
+                    MessageBox.Show("Failed to change resolution.");
+                }
+            }
+        }
+
+        /// <summary>
+        /// Event handler for the "Set 1920x1080" button.
+        /// Part of WG-ResoMate: A tool to toggle between screen resolutions.
+        /// </summary>
+        /// @since 2025-02-18
+        private void btn1080p_Click(object sender, EventArgs e)
+        {
+            ChangeResolution(1920, 1080); // Change resolution to 1920x1080
+        }
+
+        /// <summary>
+        /// Event handler for the "Set 4K" button.
+        /// Part of WG-ResoMate: A tool to toggle between screen resolutions.
+        /// </summary>
+        /// @since 2025-02-18
+        private void btn4K_Click(object sender, EventArgs e)
+        {
+            ChangeResolution(3840, 2160); // Change resolution to 3840x2160 (4K)
+        }
+    }
+}
